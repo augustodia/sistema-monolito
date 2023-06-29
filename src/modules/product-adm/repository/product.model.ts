7f10d5fe67ae87112fe6a08657aba8b6a16ffa57
Luiz Augusto <augustodia97@gmail.com>
2023-06-28 22:22:33 -0300

    ✨ typing model

--- src/modules/product-adm/repository/product.model.ts
+++ src/modules/product-adm/repository/product.model.ts
@@ -1,15 +1,31 @@
-import {Column, Model, PrimaryKey, Table} from "sequelize-typescript";
+import {Column, Model, PrimaryKey, Table, Validate} from "sequelize-typescript";
+import {InferAttributes, InferCreationAttributes, Optional} from "sequelize";
+
+export type ProductColumns = {
+    id: string;
+    name: string;
+    description: string;
+    purchasePrice: number;
+    stock: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+interface ProductCreationAttributes extends Optional<ProductColumns, 'id'> {
+    name: string;
+}
 
 @Table({
     tableName: 'products',
     timestamps: false,
 })
-export class ProductModel extends Model {
+export class ProductModel extends Model<ProductColumns, ProductCreationAttributes>{
     @PrimaryKey
     @Column({allowNull: false})
-    id: string;
+    ids: string;
 
     @Column({allowNull: false})
+    @Validate({notEmpty: true})
     name: string;
 
     @Column({allowNull: false})
