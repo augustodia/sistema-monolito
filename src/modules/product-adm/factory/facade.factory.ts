bf5d1edc8b6c9dfbc2783b55e02be5f0b9beacd3
Luiz Augusto <augustodia97@gmail.com>
2023-07-03 21:55:43 -0300

    ✨ add checkStockUseCase in ProductAdminFacadeFactory

--- src/modules/product-adm/factory/facade.factory.ts
+++ src/modules/product-adm/factory/facade.factory.ts
@@ -1,12 +1,17 @@
 import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../use-case/add-product/add-product.use-case";
 import ProductAdminFacade from "../facade/product-admin.facade";
+import CheckStockUseCase from "../use-case/check-stock/check-stock.use-case";
 
 export default class ProductAdminFacadeFactory {
     static create() {
         const productRepository = new ProductRepository();
         const addProductUseCase = new AddProductUseCase(productRepository);
-        const productFacade = new ProductAdminFacade({addUseCase: addProductUseCase, stockUseCase: undefined});
+        const checkStockUseCase = new CheckStockUseCase(productRepository);
+        const productFacade = new ProductAdminFacade(
+            {
+                addUseCase: addProductUseCase, stockUseCase: checkStockUseCase
+            });
 
         return productFacade;
     }
